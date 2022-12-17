9ad9fcd722cc5a6324dc9ed218db90529374e498
andrea <andreacantarini2015@gmail.com>
2022-12-17 18:05:00 +0100

    Added Locale changes

--- src/main/java/it/ispw22/calculator/CalculatorApplication.java
+++ src/main/java/it/ispw22/calculator/CalculatorApplication.java
@@ -5,12 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Locale;
 import java.util.Objects;
 
 public class CalculatorApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Locale.setDefault(Locale.US);
+        DecimalFormat df = new DecimalFormat("#.######");
+        df.setRoundingMode(RoundingMode.CEILING);
+
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("calculator.fxml")));
         Scene scene = new Scene(root);
         primaryStage.setTitle("Calculator");
